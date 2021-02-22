acb602b5ad920a9fb073290e8684659af49e9088
danieloprado <danielprado.ad@gmail.com>
2021-02-22 14:37:02 -0300

    Forms readme
    Signed-off-by: danieloprado <danielprado.ad@gmail.com>

--- packages/forms/useForm.tsx
+++ packages/forms/useForm.tsx
@@ -3,11 +3,12 @@ import './yupLocale';
 import { useCallback, useEffect, useRef } from 'react';
 import { useObservable } from 'react-use-observable';
 
+import { Observable, of, Subject } from 'rxjs';
+import { catchError, share, switchMap } from 'rxjs/operators';
+
 import IFormAdapter from '@eduzz/houston-core/formAdapter';
 
 import { FormikConfig, FormikErrors, FormikHelpers, FormikTouched, useFormik } from 'formik';
-import { Observable, of, Subject } from 'rxjs';
-import { catchError, share, switchMap } from 'rxjs/operators';
 import * as yup from 'yup';
 
 export declare type FormikInstance<Values = any> = ReturnType<typeof useForm> & {
@@ -29,6 +30,10 @@ export interface IUseFormParams<Values> {
   onSubmit: (values: Values, formikHelpers: FormikHelpers<Values>) => void | Promise<any> | Observable<any>;
 }
 
+/**
+ * Hook implemation of IFormAdapter
+ * @param IUseFormParams
+ */
 export default function useForm<Values = {}>({
   onSubmit,
   onSubmitWithErrors,
