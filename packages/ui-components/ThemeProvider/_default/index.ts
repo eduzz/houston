62b6249983a9edc8007683bbb35fbface13aeb17
danieloprado <danielprado.ad@gmail.com>
2021-02-25 11:31:17 -0300

    remove console.logs
    Signed-off-by: danieloprado <danielprado.ad@gmail.com>

--- packages/ui-components/ThemeProvider/_default/index.ts
+++ packages/ui-components/ThemeProvider/_default/index.ts
@@ -9,14 +9,12 @@ import themeVariable from './variables';
 
 export default function generateTheme(customPalette?: PaletteOptions) {
   const palette = createPalette({ ...defaultPalette, ...customPalette });
-  console.log('teste');
   return createMuiTheme({
     palette,
     overrides: overrides(palette),
     props,
     typography,
     spacing: (factor: number) => {
-      console.log({ factor, result: themeVariable.spacing(factor) });
       return themeVariable.spacing(factor);
     }
   });
