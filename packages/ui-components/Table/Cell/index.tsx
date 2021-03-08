5e7e0b5da9d1e2a238cd9280c075d9f6b741b61f
Fernando Moraes <fernando.moraees@eduzz.com>
2021-03-08 14:38:47 -0300

    add onclick and ondoubleclick in table row and table cell

--- packages/ui-components/Table/Cell/index.tsx
+++ packages/ui-components/Table/Cell/index.tsx
@@ -2,7 +2,15 @@ import * as React from 'react';
 
 import { TableCellProps } from '@material-ui/core/TableCell';
 
-type ITableCellExtends = 'id' | 'className' | 'children' | 'padding' | 'colSpan' | 'align';
+type ITableCellExtends =
+  | 'id'
+  | 'className'
+  | 'children'
+  | 'padding'
+  | 'colSpan'
+  | 'align'
+  | 'onClick'
+  | 'onDoubleClick';
 
 export interface ITableCellProps extends Pick<TableCellProps, ITableCellExtends> {
   /**
