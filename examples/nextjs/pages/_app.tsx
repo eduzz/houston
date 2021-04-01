755ee783de8375396b8e1cc9c476f217ddb01ff0
Fernando Moraes <fernando.moraees@eduzz.com>
2021-04-01 11:52:52 -0300

    Projeto de implementação do Houston com NextJS.

---
+++ examples/nextjs/pages/_app.tsx
@@ -0,0 +1,26 @@
+import '../assets/styles/global.scss';
+import 'react-toastify/dist/ReactToastify.css';
+
+import { useEffect } from 'react';
+
+import ThemeProvider from '@eduzz/houston-ui/ThemeProvider';
+
+import { AppProps } from 'next/dist/next-server/lib/router/router';
+
+function App({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    const styles = document.querySelector('#jss-server-side');
+
+    if (styles) {
+      styles?.parentElement?.removeChild(styles);
+    }
+  }, []);
+
+  return (
+    <ThemeProvider>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
+}
+
+export default App;
