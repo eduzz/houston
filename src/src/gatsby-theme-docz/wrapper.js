33d83c1c1623618a6d0b8ffd76f9cbcd43af93fa
danieloprado <danielprado.ad@gmail.com>
2021-04-10 12:45:03 -0300

    fix headers
    Signed-off-by: danieloprado <danielprado.ad@gmail.com>

--- src/src/gatsby-theme-docz/wrapper.js
+++ src/src/gatsby-theme-docz/wrapper.js
@@ -3,7 +3,7 @@ import React from 'react';
 import { StylesProvider, createGenerateClassName } from '@material-ui/core/styles';
 
 const generateClassName = createGenerateClassName({
-  disableGlobal: true
+  disableGlobal: false
 });
 
 export default ({ children }) => {
