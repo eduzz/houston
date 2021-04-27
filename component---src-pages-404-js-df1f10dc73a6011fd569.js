530ae03b5a0298f57c2abce51367f16e89248490
danieloprado <danielprado.ad@gmail.com>
2021-04-27 18:06:49 +0000

    Update gh-pages to output generated at 2e9e0af

--- component---src-pages-404-js-df1f10dc73a6011fd569.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[28],{w2l6:function(t,e,n){"use strict";n.r(e);var i=n("l1C2");e.default=function(){return Object(i.c)("div",{style:{display:"flex",alignItems:"center",justifyContent:"center",height:"100vh",width:"100vw",fontSize:32}},"Not Found")}}}]);
-//# sourceMappingURL=component---src-pages-404-js-df1f10dc73a6011fd569.js.map
