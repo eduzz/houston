8bc6675da914095e124c20d4e0a4bd2950d21f1b
Fernando Moraes <fernando.moraees@eduzz.com>
2021-04-29 14:27:42 -0300

    Simplificando o wrapperWeb

--- src/packages/icons/wrapperWeb.tsx
+++ src/packages/icons/wrapperWeb.tsx
@@ -4,8 +4,13 @@ import { IIconWebBase } from './interfaces';
 import getIconSize from './utils/getIconSize';
 
 const IconBase = React.memo<IIconWebBase>(({ children, size, ...rest }) => {
-  const width = React.useMemo(() => getIconSize(size), [size]);
-  const child = React.useMemo(() => React.cloneElement(children as React.ReactElement, { width }), [children, width]);
+  const child = React.useMemo(
+    () =>
+      React.cloneElement(children as React.ReactElement, {
+        width: getIconSize(size)
+      }),
+    [children, size]
+  );
 
   return (
     <span className='houston-icon' {...rest}>
