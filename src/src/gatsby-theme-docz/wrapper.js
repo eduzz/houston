7690ed541d8c1229e7b7496e0aa31aaa6b0ab1d2
danieloprado <danielprado.ad@gmail.com>
2021-05-10 11:38:15 -0300

    fix render layout
    Signed-off-by: danieloprado <danielprado.ad@gmail.com>

--- src/src/gatsby-theme-docz/wrapper.js
+++ src/src/gatsby-theme-docz/wrapper.js
@@ -4,6 +4,16 @@ import React from 'react';
 
 import { StylesProvider } from '@material-ui/core/styles';
 
+
+function fixLayout() {
+  const aButton = document.querySelector('.sidebar a');
+  if(!aButton) return;
+  aButton.click();
+  setTimeout(() => history.back(), 100);
+}
+fixLayout();
+
+
 const generateClassName = (rule, styleSheet) => {
   const random = Math.random().toString(36).substring(5);
   return `${styleSheet.options.classNamePrefix}-${rule.key}-${random}`;
