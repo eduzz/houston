64124edd353472e0b51f5e1ed937d94773eb1e55
danilolucasmd <danilolucasmd@gmail.com>
2021-06-26 13:55:06 -0300

    Remove Animated.ScrollView and use ScrollView instead for compatibility reasons

--- src/dev-mobile/App.tsx
+++ src/dev-mobile/App.tsx
@@ -39,66 +39,6 @@ export default function App() {
           color='#fff'
           iconElement={<Image style={styles.icon} source={notificationsOff} />}
         />
-        <ActionSheet.Item
-          title='Desativar'
-          description='Pare de receber notificações deste curso'
-          color='#fff'
-          iconElement={<Image style={styles.icon} source={notificationsOff} />}
-        />
-        <ActionSheet.Item
-          title='Desativar'
-          description='Pare de receber notificações deste curso'
-          color='#fff'
-          iconElement={<Image style={styles.icon} source={notificationsOff} />}
-        />
-        <ActionSheet.Item
-          title='Desativar'
-          description='Pare de receber notificações deste curso'
-          color='#fff'
-          iconElement={<Image style={styles.icon} source={notificationsOff} />}
-        />
-        <ActionSheet.Item
-          title='Desativar'
-          description='Pare de receber notificações deste curso'
-          color='#fff'
-          iconElement={<Image style={styles.icon} source={notificationsOff} />}
-        />
-        <ActionSheet.Item
-          title='Desativar'
-          description='Pare de receber notificações deste curso'
-          color='#fff'
-          iconElement={<Image style={styles.icon} source={notificationsOff} />}
-        />
-        <ActionSheet.Item
-          title='Desativar'
-          description='Pare de receber notificações deste curso'
-          color='#fff'
-          iconElement={<Image style={styles.icon} source={notificationsOff} />}
-        />
-        <ActionSheet.Item
-          title='Desativar'
-          description='Pare de receber notificações deste curso'
-          color='#fff'
-          iconElement={<Image style={styles.icon} source={notificationsOff} />}
-        />
-        <ActionSheet.Item
-          title='Desativar'
-          description='Pare de receber notificações deste curso'
-          color='#fff'
-          iconElement={<Image style={styles.icon} source={notificationsOff} />}
-        />
-        <ActionSheet.Item
-          title='Desativar'
-          description='Pare de receber notificações deste curso'
-          color='#fff'
-          iconElement={<Image style={styles.icon} source={notificationsOff} />}
-        />
-        <ActionSheet.Item
-          title='Desativar'
-          description='Pare de receber notificações deste curso'
-          color='#fff'
-          iconElement={<Image style={styles.icon} source={notificationsOff} />}
-        />
       </ActionSheet>
     </View>
   );
--- src/packages/mobile-components/ActionSheet/index.tsx
+++ src/packages/mobile-components/ActionSheet/index.tsx
@@ -9,7 +9,8 @@ import {
   NativeSyntheticEvent,
   SafeAreaView,
   StyleSheet,
-  View
+  View,
+  ScrollView
 } from 'react-native';
 
 import ActionItem from './ActionItem';
@@ -106,7 +107,7 @@ const ActionSheet = ({ visible, backgroundColor, onRequestClose, onFinishClosing
     <Modal animationType='none' visible={localVisible} transparent supportedOrientations={['portrait', 'landscape']}>
       <SafeAreaView style={styles.container} onTouchEnd={onRequestClose}>
         <Animated.View style={[styles.backdrop, { opacity }]} />
-        <Animated.ScrollView
+        <ScrollView
           ref={scrollViewRef}
           style={styles.content}
           overScrollMode='never'
@@ -129,7 +130,7 @@ const ActionSheet = ({ visible, backgroundColor, onRequestClose, onFinishClosing
             {children}
             <View style={{ height: 100 }} />
           </View>
-        </Animated.ScrollView>
+        </ScrollView>
       </SafeAreaView>
     </Modal>
   );
