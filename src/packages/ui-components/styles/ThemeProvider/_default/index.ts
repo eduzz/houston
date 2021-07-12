87f05ddb08c03a4dba9c7c86a78010cea42f1235
Matheus de Lima Mendes <matheus.mendes@eduzz.com>
2021-07-12 11:53:29 -0300

    corrige o import do createTheme do MUI no ThemeProvider

--- src/packages/ui-components/styles/ThemeProvider/_default/index.ts
+++ src/packages/ui-components/styles/ThemeProvider/_default/index.ts
@@ -1,6 +1,6 @@
 /* eslint-disable @typescript-eslint/naming-convention */
-import createMuiTheme from '@material-ui/core/styles/createMuiTheme';
 import createPalette, { Palette } from '@material-ui/core/styles/createPalette';
+import createTheme from '@material-ui/core/styles/createTheme';
 
 import overrides from './overrides';
 import props from './props';
@@ -8,7 +8,7 @@ import typography from './typography';
 import defaultThemeVariables from './variables';
 import themeVariable from './variables';
 
-declare module '@material-ui/core/styles/createMuiTheme' {
+declare module '@material-ui/core/styles/createTheme' {
   interface Theme {
     houston?: typeof defaultThemeVariables & { colors: Palette };
   }
@@ -21,7 +21,7 @@ declare module '@material-ui/core/styles/createMuiTheme' {
 export default function generateTheme(customPalette?: Partial<typeof themeVariable.colors>) {
   const palette = createPalette({ ...themeVariable.colors, ...customPalette });
 
-  return createMuiTheme({
+  return createTheme({
     palette,
     overrides: overrides(palette),
     props,
