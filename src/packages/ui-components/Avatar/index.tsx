20eefd288e4af0b1f0614ab32dbe2d89359fce6e
Fernando Moraes <fernando.moraees@eduzz.com>
2021-07-26 12:49:46 -0300

    Componente Avatar

---
+++ src/packages/ui-components/Avatar/index.tsx
@@ -0,0 +1,141 @@
+import * as React from 'react';
+
+import AvatarMUI, { AvatarProps } from '@material-ui/core/Avatar';
+import makeStyles from '@material-ui/core/styles/makeStyles';
+import clsx from 'clsx';
+
+import AvatarOutline from '@eduzz/houston-icons/AvatarOutline';
+import AvatarSolid from '@eduzz/houston-icons/AvatarSolid';
+
+import withHoustonTheme from '../styles/ThemeProvider/WrapperTheme';
+
+type AvatarPropsExtends = 'id' | 'className' | 'src' | 'alt' | 'onClick';
+
+export type IAvatarSize = 'small' | 'middle' | 'large' | number;
+
+export type IAvatarType = 'text' | 'icon';
+
+export interface IAvatarProps extends Pick<AvatarProps, AvatarPropsExtends> {
+  /**
+   * Default `false`
+   */
+  filled?: boolean;
+  /**
+   * Default `icon`
+   */
+  type?: IAvatarType;
+  /**
+   * Default `middle`
+   */
+  size?: IAvatarSize;
+}
+
+const sizes = { small: 40, middle: 60, large: 80 };
+
+const useStyles = makeStyles(theme => ({
+  root: ({ size }: { size: IAvatarSize }) => ({
+    display: 'inline-flex',
+    background: 'none',
+    border: `2px solid ${theme.palette.grey[500]}`,
+    color: theme.palette.grey[500],
+    fontWeight: theme.houston.fontWeight('semibold'),
+    width: size,
+    height: size,
+
+    '&.--icon': {
+      alignItems: 'flex-end',
+      borderWidth: 2,
+
+      '& span.houston-icon': {
+        position: 'relative',
+        top: 5
+      }
+    },
+
+    '&.--filled': {
+      background: theme.palette.grey[500],
+      color: 'white',
+      borderWidth: 4,
+
+      '& span.houston-icon': {
+        color: 'white',
+        top: '7px !important'
+      }
+    },
+
+    '&.--image': {
+      borderWidth: 1
+    }
+  }),
+
+  small: ({ size }: { size: IAvatarSize }) => ({
+    width: size ?? sizes.small,
+    height: size ?? sizes.small,
+    fontSize: theme.houston.textSize('small'),
+    borderWidth: 2
+  }),
+
+  middle: ({ size }: { size: IAvatarSize }) => ({
+    width: size ?? sizes.middle,
+    height: size ?? sizes.middle,
+    fontSize: theme.houston.textSize('medium')
+  }),
+
+  large: ({ size }: { size: IAvatarSize }) => ({
+    width: size ?? sizes.large,
+    height: size ?? sizes.large,
+    fontSize: theme.houston.textSize('x-large')
+  })
+}));
+
+const Avatar: React.FC<IAvatarProps> = ({
+  children,
+  className,
+  size = 'middle',
+  type = 'icon',
+  filled,
+  src,
+  ...rest
+}) => {
+  const hasIcon = React.useMemo(() => type === 'icon', [type]);
+  const sizeIsNumber = React.useMemo(() => typeof size === 'number', [size]);
+
+  const classes = useStyles({ size: sizeIsNumber ? size : null });
+
+  const sizeClasses = React.useMemo(
+    () => ({ small: classes.small, middle: classes.middle, large: classes.large }),
+    [classes.large, classes.middle, classes.small]
+  );
+
+  const iconRender = React.useMemo(() => {
+    const currentSize = (sizeIsNumber ? size : sizes[size]) - 4;
+
+    if (filled) {
+      return <AvatarSolid size={currentSize} />;
+    }
+
+    return <AvatarOutline size={currentSize} />;
+  }, [filled, size, sizeIsNumber]);
+
+  const styles = React.useMemo(
+    () =>
+      clsx(
+        classes.root,
+        !sizeIsNumber && sizeClasses[size],
+        hasIcon && '--icon',
+        filled && '--filled',
+        src && '--image',
+        className
+      ),
+    [className, classes.root, filled, hasIcon, size, sizeClasses, sizeIsNumber, src]
+  );
+
+  return (
+    <AvatarMUI {...rest} src={src} className={styles}>
+      {hasIcon && iconRender}
+      {type === 'text' && children}
+    </AvatarMUI>
+  );
+};
+
+export default withHoustonTheme(React.memo(Avatar));
--- src/packages/ui-components/Table/Action/index.tsx
+++ src/packages/ui-components/Table/Action/index.tsx
@@ -5,20 +5,19 @@ import { useContextSelector } from 'use-context-selector';
 import TableRowContext from '../Row/context';
 
 export interface ITableActionOptionProp {
-  children: React.ReactNode;
+  children?: React.ReactNode;
   onClick: (data: unknown, index?: number) => void;
   icon?: React.ReactNode;
   disabled?: boolean;
-  order?: number;
 }
 
-const TableActionOption = React.memo<ITableActionOptionProp>(({ children, disabled, onClick, order, icon }) => {
+const TableActionOption = React.memo<ITableActionOptionProp>(({ children, disabled, onClick, icon }) => {
   const registerAction = useContextSelector(TableRowContext, context => context.registerAction);
 
   React.useEffect(() => {
     const unregister = registerAction({ disabled, onClick, icon, content: children });
     return () => unregister();
-  }, [children, disabled, onClick, icon, order, registerAction]);
+  }, [children, disabled, onClick, icon, registerAction]);
 
   return null;
 });
