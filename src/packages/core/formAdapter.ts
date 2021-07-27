c7c3e6c6854503cede5501631e07e1f7c491f5ca
Fernando Moraes <fernando.moraees@eduzz.com>
2021-07-27 15:11:34 -0300

    Atualizações na documentação e adicionando errors no useForm

--- src/packages/core/formAdapter.ts
+++ src/packages/core/formAdapter.ts
@@ -1,3 +1,14 @@
+/* eslint-disable @typescript-eslint/ban-types */
+type IFormikErrors<Values> = {
+  [K in keyof Values]?: Values[K] extends any[]
+    ? Values[K][number] extends object
+      ? IFormikErrors<Values[K][number]>[] | string | string[]
+      : string | string[]
+    : Values[K] extends object
+    ? IFormikErrors<Values[K]>
+    : string;
+};
+
 export default interface IFormAdapter<V extends { [key: string]: any }> {
   handleSubmit?: (event: any) => void;
   handleChange?: (name: string) => (value: any) => void;
@@ -12,4 +23,5 @@ export default interface IFormAdapter<V extends { [key: string]: any }> {
   isSubmitting: boolean;
   isValid: boolean;
   reset: (values?: V) => void;
+  errors: IFormikErrors<Partial<V>>;
 }
