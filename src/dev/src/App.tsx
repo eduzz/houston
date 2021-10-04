0164dd3bca3544dd2f9fdd21b75ebf26edda7731
Fernando Moraes <fernando.moraees@eduzz.com>
2021-10-04 16:51:34 -0300

    Removendo código de teste

--- src/dev/src/App.tsx
+++ src/dev/src/App.tsx
@@ -1,22 +1,12 @@
 import React from 'react';
 
-import ThemeProvider, { IHoustonPalette } from '@eduzz/houston-ui/styles/ThemeProvider';
+import ThemeProvider from '@eduzz/houston-ui/styles/ThemeProvider';
 
 import ComponentDev from './components';
 
 function App() {
-  const theme: Partial<IHoustonPalette> = {
-    // mode
-    type: 'dark',
-    // custom
-    variables: {
-      foo: true,
-      bar: false
-    }
-  };
-
   return (
-    <ThemeProvider palette={theme}>
+    <ThemeProvider>
       <ComponentDev />
     </ThemeProvider>
   );
