33f1278ce2e4251723487973c3ea73d7b86e3868
Fernando Moraes <fernando.moraees@eduzz.com>
2021-10-20 16:40:44 -0300

    style(doc): melhorias nos estilos

--- src/pages/_app.page.jsx
+++ src/pages/_app.page.jsx
@@ -5,7 +5,7 @@ import { DokzProvider, GithubLink } from 'dokz';
 import Head from 'next/head';
 
 import ToastContainer from '@eduzz/houston-ui/Toast/Container';
-import './styles.css';
+import './_app.page.css';
 
 const sidebarOrdering = {
   'index.mdx': true,
@@ -14,14 +14,21 @@ const sidebarOrdering = {
 
 export default function App(props) {
   const { Component, pageProps } = props;
+
   return (
     <ChakraProvider resetCSS>
       <Head>
         <link href='https://fonts.googleapis.com/css?family=Fira+Code' rel='stylesheet' key='google-font-Fira' />
+        <link href='https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;600;700' rel='stylesheet' />
       </Head>
+
       <ToastContainer />
+
       <DokzProvider
         animate
+        footer={<footer className='houston-footer'>Houston - Feito com ❤️ pela Eduzz.</footer>}
+        headTitlePrefix='Houston | '
+        fontFamily='Open Sans'
         headerLogo={<img src='/houston/img/logo.svg' height='60px' />}
         headerItems={[<GithubLink key='0' url='https://github.com/remorses/dokz' />]}
         sidebarOrdering={sidebarOrdering}
