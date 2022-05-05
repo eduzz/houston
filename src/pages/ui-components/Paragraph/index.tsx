2a85cbebed563b1d91831da4c542482383f25840
Miguel <miguelaugl@outlook.com>
2022-05-05 17:41:32 -0300

    feat(paragraph): add Paragraph component

---
+++ src/pages/ui-components/Paragraph/index.tsx
@@ -0,0 +1,60 @@
+import * as React from 'react';
+
+import { IStyledProp } from '@eduzz/houston-styles/styled';
+import useHoustonTheme from '@eduzz/houston-styles/useHoustonTheme';
+import type { HoustonTokens } from '@eduzz/houston-tokens';
+
+import useMediaQuery from '../hooks/useMediaQuery';
+import Typography from '../Typography';
+
+type ParagraphTags = 'p' | 'span' | 'strong';
+
+export type ParagraphSizes = 'large' | 'small';
+
+export interface IHeadingProps extends IStyledProp {
+  id?: string;
+  /**
+   * Defaults to 'large'
+   */
+  size?: ParagraphSizes;
+  weight?: keyof HoustonTokens['font']['weight'];
+  lineHeight?: keyof HoustonTokens['line']['height'];
+  marginBottom?: boolean;
+  children?: React.ReactNode;
+  onClick?: (e: React.MouseEvent) => void;
+  /**
+   * Defaults to 'p'
+   */
+  as?: ParagraphTags;
+  ['aria-label']?: string;
+}
+
+type SizesMap = {
+  [key in ParagraphSizes]: keyof HoustonTokens['font']['size'];
+};
+
+const defaultSizesMap: SizesMap = {
+  large: 'lg',
+  small: 'md'
+};
+
+const mobileSizesMap: SizesMap = {
+  large: 'sm',
+  small: 'xs'
+};
+
+const Paragraph = React.forwardRef<HTMLParagraphElement | HTMLSpanElement | HTMLElement, IHeadingProps>(
+  ({ as = 'p', children, size: sizeProp = 'large', ...props }, ref) => {
+    const { breakpoints } = useHoustonTheme();
+    const isMobile = useMediaQuery(breakpoints.down('sm'));
+    const sizesMap = isMobile ? mobileSizesMap : defaultSizesMap;
+    const fontSize = sizesMap[sizeProp];
+    return (
+      <Typography as={as} ref={ref} size={fontSize} {...props}>
+        {children}
+      </Typography>
+    );
+  }
+);
+
+export default Paragraph;
