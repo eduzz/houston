562ded1a238d1a9a194d84ff80f2f4a75c49bc36
Fernando <fermoraeslima10@gmail.com>
2022-05-11 13:44:32 -0300

    chore(avatar): improvements, refactoring

--- src/pages/tokens/variables/utils.ts
+++ src/pages/tokens/variables/utils.ts
@@ -1,12 +1,24 @@
 import { PxToRem } from '../types';
 
-export const pxToRem: PxToRem = (value: number | string, rate = 16): string => {
-  if (typeof value === 'string') {
-    value = value.replace(/\D/g, '');
-    value = value && parseInt(value);
+export const cleanValue = (value: string) => {
+  if (!/px$/g.test(value)) {
+    console.warn(
+      "[@eduzz/houston-tokens]: use valores em pixels (px) na função pxToRem(). Por exemplo: pxToRem('16px')"
+    );
 
-    return `${value / rate}rem`;
+    return NaN;
   }
 
-  return `${value / rate}rem`;
+  value = value.replace(/\D/g, '');
+  return value && parseInt(value);
+};
+
+export const pxToRem: PxToRem = (value: string, rate = 16) => {
+  const cleanedValue = cleanValue(value);
+
+  if (!cleanedValue) {
+    return value;
+  }
+
+  return `${cleanedValue / rate}rem`;
 };
--- src/pages/ui-components/Avatar/index.tsx
+++ src/pages/ui-components/Avatar/index.tsx
@@ -6,11 +6,11 @@ export type IAvatarSize = 'xs' | 'sm' | 'md' | 'lg';
 
 export type IAvatarColor = 'primary' | 'high';
 
-type ISizesMap = { [key in IAvatarSize]: number };
+type ISizesMap = { [key in IAvatarSize]: string };
 
 type IFontSizesMap = { [key in IAvatarSize]: string };
 
-const sizesMap: ISizesMap = { xs: 24, sm: 32, md: 40, lg: 64 };
+const sizesMap: ISizesMap = { xs: '24px', sm: '32px', md: '40px', lg: '64px' };
 
 export interface IAvatarProps extends React.HTMLAttributes<HTMLDivElement>, IStyledProp {
   src?: string;
@@ -23,36 +23,33 @@ export interface IAvatarProps extends React.HTMLAttributes<HTMLDivElement>, ISty
    * Default `md`
    */
   size?: IAvatarSize;
+  children?: React.ReactNode;
 }
 
-const Avatar: React.FC<IAvatarProps> = ({ src, alt, children, className, color = 'primary', id, onClick }) => {
-  const wrapperProps = React.useMemo(() => ({ id, onClick }), [id, onClick]);
-  const imageProps = React.useMemo(() => ({ src, alt }), [src, alt]);
+const Avatar = ({ src, alt, children, className, color = 'primary', id, onClick }: IAvatarProps) => {
+  const wrapperProps = { id, onClick };
+  const imageProps = { src, alt };
 
   const hasImage = !!src;
   const hasText = children && typeof children === 'string';
 
-  const firstLetter = React.useMemo(() => (hasText && children?.[0]?.trim()) ?? ' ', [children, hasText]);
+  const firstLetter = (hasText && children?.[0]?.trim()) ?? ' ';
 
   return (
-    <div className={cx(className, { '--high': color === 'high' })} {...wrapperProps}>
-      {hasImage && (
-        <figure className='__image'>
-          <img {...imageProps} />
-        </figure>
-      )}
+    <span className={cx(className, { '--high': color === 'high' })} {...wrapperProps}>
+      {hasImage && <img {...imageProps} />}
 
-      {!hasImage && hasText && <div className='__text'>{firstLetter}</div>}
+      {!hasImage && hasText && <span className='__text'>{firstLetter}</span>}
 
       {!hasImage && !hasText && (
-        <div className='__icon'>
-          <svg viewBox='0 0 44 55' fill='none' xmlns='http://www.w3.org/2000/svg'>
+        <span className='__icon' role='img' aria-label='usuário'>
+          <svg viewBox='0 0 44 55' fill='none' xmlns='http://www.w3.org/2000/svg' focusable={false} aria-hidden='true'>
             <path d='M43.3333 45.1362C38.1975 51.1715 30.5458 55 22 55C13.4542 55 5.80257 51.1715 0.666687 45.1362V40.3333C0.666687 33.7059 6.03927 28.3333 12.6667 28.3333H31.3333C37.9608 28.3333 43.3333 33.7059 43.3333 40.3333V45.1362Z' />
             <path d='M22 24.3333C28.6274 24.3333 34 18.9608 34 12.3333C34 5.70592 28.6274 0.333336 22 0.333336C15.3726 0.333336 10 5.70592 10 12.3333C10 18.9608 15.3726 24.3333 22 24.3333Z' />
           </svg>
-        </div>
+        </span>
       )}
-    </div>
+    </span>
   );
 };
 
@@ -75,22 +72,19 @@ export default styled(React.memo(Avatar), { label: 'houston-avatar' })(({ theme,
     justify-content: center;
     background-color: ${theme.brandColor.primary.pure};
 
+    img {
+      width: 100%;
+      height: 100%;
+      display: block;
+      object-fit: cover;
+    }
+
     &.--high {
       color: ${theme.brandColor.primary.pure};
       background-color: ${theme.neutralColor.high.pure};
 
       .__icon svg {
         fill: ${theme.brandColor.primary.pure};
-      }
-    }
-
-    .__image {
-      img,
-      svg {
-        width: 100%;
-        height: 100%;
-        display: block;
-        object-fit: cover;
       }
     }
 
