cd8f677f33ec28a8860bd3418f6c6c3b78634dd8
Fernando <fermoraeslima10@gmail.com>
2022-05-31 14:27:41 -0300

    fix(layout): exposed interface

---
+++ src/pages/ui-components/Layout/Layout.tsx
@@ -0,0 +1,24 @@
+import * as React from 'react';
+
+import styled from '@emotion/styled';
+
+import { IStyledProp } from '@eduzz/houston-styles/styled';
+
+import nestedComponent from '../utils/nestedComponent';
+import Content from './Content';
+import Sidebar from './Sidebar';
+
+export interface ILayoutProps extends IStyledProp {
+  children?: React.ReactNode;
+}
+
+const Layout = ({ ...rest }: ILayoutProps) => <div {...rest} />;
+
+const LayoutWrapper = styled(Layout, { label: 'houston-layout' })`
+  display: flex;
+`;
+
+export default nestedComponent(LayoutWrapper, {
+  Sidebar,
+  Content
+});
--- src/pages/ui-components/Layout/Sidebar/index.tsx
+++ src/pages/ui-components/Layout/Sidebar/index.tsx
@@ -1,6 +1,7 @@
 import { SIDEBAR_WIDTH, SIDEBAR_WIDTH_COLLAPSED } from './context';
-import Sidebar from './Sidebar';
+import Sidebar, { ISidebarProps as SidebarProps } from './Sidebar';
 
 export { SIDEBAR_WIDTH, SIDEBAR_WIDTH_COLLAPSED };
+export type ISidebarProps = SidebarProps;
 
 export default Sidebar;
