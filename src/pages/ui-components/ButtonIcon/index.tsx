4e5618d3181de87760b51f2d74251da3c45298a6
phelpa <pliniohelpa@gmail.com>
2022-06-10 16:23:38 -0300

    feat(IconButton): add disabled prop, remove small prop

--- src/pages/ui-components/ButtonIcon/index.tsx
+++ src/pages/ui-components/ButtonIcon/index.tsx
@@ -5,15 +5,20 @@ export interface IIconButtonProps
   extends React.ButtonHTMLAttributes<HTMLButtonElement>,
     React.RefAttributes<HTMLButtonElement>,
     IStyledProp {
-  size?: 'large' | 'medium' | 'small';
+  size?: 'large' | 'medium';
 }
 
 const IconButton: React.FC<IIconButtonProps> = props => {
-  const { className, size, children, ...rest } = props;
-
+  const { className, size, children, disabled = false, ...rest } = props;
   return (
-    <button className={cx(className, `--${size ?? 'large'}`)} role='button' {...rest}>
-      <div className={cx('__icon', `--${size ?? 'large'}`)}>{children}</div>
+    <button
+      role='button'
+      disabled={disabled}
+      className={cx(className, `--${size ?? 'large'}`)}
+      {...(disabled && { 'aria-disabled': true })}
+      {...rest}
+    >
+      <div className={cx('__icon', `--${size ?? 'large'}`, { '--disabled': disabled })}>{children}</div>
     </button>
   );
 };
@@ -24,14 +29,13 @@ const LG_ICON_SIZE = 24;
 const MD_SIZE = 32;
 const MD_ICON_SIZE = 16;
 
-//1rem = 16px;
 export default React.memo(styled(IconButton, { label: 'houston-icon-button' })`
   ${({ theme }) => css`
     border: none;
     background-color: ${theme.hexToRgba(theme.neutralColor.low.pure, theme.opacity.level[0])};
     border-radius: ${theme.border.radius.circular};
     cursor: pointer;
-    display: flex;
+    display: inline-flex;
     align-items: center;
     justify-content: center;
 
@@ -45,10 +49,18 @@ export default React.memo(styled(IconButton, { label: 'houston-icon-button' })`
       height: ${theme.pxToRem(MD_SIZE)}rem;
     }
 
+    &:disabled {
+      cursor: default;
+    }
+
     .__icon {
       display: flex;
       align-items: center;
       justify-content: center;
+
+      &.--disabled {
+        color: ${theme.hexToRgba(theme.neutralColor.low.pure, theme.opacity.level[6])};
+      }
 
       &.--large {
         width: ${theme.pxToRem(LG_ICON_SIZE)}rem;
@@ -63,6 +75,11 @@ export default React.memo(styled(IconButton, { label: 'houston-icon-button' })`
       &.--medium {
         width: ${theme.pxToRem(MD_ICON_SIZE)}rem;
         height: ${theme.pxToRem(MD_ICON_SIZE)}rem;
+
+        > svg {
+          vertical-align: middle;
+          font-size: ${theme.pxToRem(MD_ICON_SIZE)}rem;
+        }
       }
     }
 
@@ -75,6 +92,5 @@ export default React.memo(styled(IconButton, { label: 'houston-icon-button' })`
       transition: 0.3s background-color;
     }
   
-
   `}
 `);
