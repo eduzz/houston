f5713ec78f3e329d0b19622209f905dfca660bb8
Fernando <fermoraeslima10@gmail.com>
2022-07-05 14:00:21 -0300

    chore: save code

--- src/pages/ui-components/hooks/useOverlay.tsx
+++ src/pages/ui-components/hooks/useOverlay.tsx
@@ -5,11 +5,18 @@ import createUseStyles from '@eduzz/houston-styles/createUseStyles';
 
 const useStyles = createUseStyles(theme => ({
   root: {
-    background: theme.neutralColor.low.pure
+    background: theme.hexToRgba(theme.neutralColor.low.pure, 0.4),
+    width: '100%',
+    height: '100%',
+    position: 'fixed',
+    top: 0,
+    left: 0,
+    zIndex: 1000,
+    animation: '0.15s linear'
   }
 }));
 
-function useOverlay() {
+function useOverlay(): [() => void, boolean] {
   const element = React.useRef<HTMLDivElement>(null);
   const classes = useStyles();
 
@@ -23,11 +30,14 @@ function useOverlay() {
       return;
     }
 
-    if (!visible) {
+    const hasElementInDocument = document.body.contains(element.current);
+
+    if (hasElementInDocument) {
+      document.body.removeChild(element.current);
     }
   }, [classes.root, visible]);
 
-  return { visible, toggle };
+  return [toggle, visible];
 }
 
 export default useOverlay;
