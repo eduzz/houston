d21faf3e6495bc914f441febeb9e533749dd8428
danieloprado <danielprado.ad@gmail.com>
2022-07-08 13:51:59 -0300

    feat(ui-components): deprecated form support

--- src/pages/ui-components/Forms/Form/withForm.tsx
+++ src/pages/ui-components/Forms/Form/withForm.tsx
@@ -3,6 +3,14 @@ import * as React from 'react';
 
 import { Controller, useFormContext } from 'react-hook-form';
 
+import {
+  useFormIsSubmitting,
+  useFormValue,
+  useFormError,
+  useFormSetValue
+} from '@eduzz/houston-forms/deprecated/context';
+import { useFormContext as useFormContextDeprecated } from '@eduzz/houston-forms/deprecated/useForm';
+
 export interface WithFormProps<R> {
   name?: string;
   errorMessage?: string;
@@ -13,9 +21,14 @@ export interface WithFormProps<R> {
 const withForm = <P extends WithFormProps<any>>(Component: React.ComponentType<P>) =>
   React.forwardRef<P['genericRef'], Omit<P, 'genericRef'>>(({ name, errorMessage, disabled, ...props }, ref) => {
     const form = useFormContext();
+    const formDeprecated = useFormContextDeprecated();
 
-    if (!form || !name) {
+    if (!form || !formDeprecated || !name) {
       return <Component {...(props as any)} name={name} errorMessage={errorMessage} ref={ref} />;
+    }
+
+    if (formDeprecated) {
+      return withFormDeprecated(Component)({ name, errorMessage, disabled, ...props } as P, ref);
     }
 
     return (
@@ -35,4 +48,29 @@ const withForm = <P extends WithFormProps<any>>(Component: React.ComponentType<P
     );
   });
 
+const withFormDeprecated =
+  <P extends WithFormProps<any>>(Component: React.ComponentType<P>) =>
+  (
+    { name, value: valueProp, errorMessage: errorMessageProp, disabled, ...props }: P & { value?: any },
+    ref: P['genericRef']
+  ) => {
+    const isSubmitting = useFormIsSubmitting();
+    const value = useFormValue(name as string, valueProp);
+    const errorMessage = useFormError(name as string, errorMessageProp);
+    const setFormValue = useFormSetValue(name as string);
+
+    const handleChange = React.useCallback((value: any) => setFormValue && setFormValue(value), [setFormValue]);
+
+    return (
+      <Component
+        {...(props as any)}
+        name={name}
+        value={value}
+        onChange={handleChange}
+        disabled={disabled || isSubmitting}
+        errorMessage={errorMessage}
+        ref={ref}
+      />
+    );
+  };
 export default withForm;
