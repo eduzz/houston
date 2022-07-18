9d734299591635ffacac93161c989ee0bf759440
phelpa <pliniohelpa@gmail.com>
2022-07-18 17:16:56 -0300

    feat(Tag): finish css structure

--- src/pages/ui-components/Tag/Avatar/index.tsx
+++ src/pages/ui-components/Tag/Avatar/index.tsx
@@ -1,5 +1,10 @@
+import styled, { css } from '@eduzz/houston-styles';
 import Avatar from '@eduzz/houston-ui/Avatar';
 
 const TagAvatar = ({ ...rest }) => <Avatar size='xs' {...rest} />;
 
-export default TagAvatar;
+export default styled(TagAvatar, { label: 'houston-tag-avatar' })(({ theme }) => {
+  return css`
+    margin-right: ${theme.spacing.inline.nano};
+  `;
+});
--- src/pages/ui-components/Tag/PlusIcon/index.tsx
+++ src/pages/ui-components/Tag/PlusIcon/index.tsx
@@ -1,18 +1,24 @@
-const PlusIcon = () => (
-  <svg xmlns='http://www.w3.org/2000/svg' width='12' height='12' viewBox='0 0 12 12' fill='none'>
+import styled, { css, StyledProp } from '@eduzz/houston-styles';
+
+const PlusIcon = ({ className }: StyledProp) => (
+  <svg className={className} xmlns='http://www.w3.org/2000/svg' width='16' height='16' viewBox='0 0 16 16' fill='none'>
     <path
       fillRule='evenodd'
       clipRule='evenodd'
-      d='M6.08331 0C6.35946 0 6.58331 0.223858 6.58331 0.5L6.58331 11.5C6.58331 11.7761 6.35946 12 6.08331 12C5.80717 12 5.58331 11.7761 5.58331 11.5L5.58331 0.5C5.58331 0.223858 5.80717 0 6.08331 0Z'
+      d='M8.08337 2C8.35952 2 8.58337 2.22386 8.58337 2.5V13.5C8.58337 13.7761 8.35952 14 8.08337 14C7.80723 14 7.58337 13.7761 7.58337 13.5L7.58337 2.5C7.58337 2.22386 7.80723 2 8.08337 2Z'
       fill='black'
     />
     <path
       fillRule='evenodd'
       clipRule='evenodd'
-      d='M0 6C0 5.72386 0.223858 5.5 0.5 5.5L11.5 5.5C11.7761 5.5 12 5.72386 12 6C12 6.27614 11.7761 6.5 11.5 6.5L0.5 6.5C0.223858 6.5 0 6.27614 0 6Z'
+      d='M2 8C2 7.72386 2.22386 7.5 2.5 7.5L13.5 7.5C13.7761 7.5 14 7.72386 14 8C14 8.27614 13.7761 8.5 13.5 8.5L2.5 8.5C2.22386 8.5 2 8.27614 2 8Z'
       fill='black'
     />
   </svg>
 );
 
-export default PlusIcon;
+export default styled(PlusIcon, { label: 'houston-tag-plus-icon' })(({ theme }) => {
+  return css`
+    margin-right: ${theme.spacing.inline.nano};
+  `;
+});
