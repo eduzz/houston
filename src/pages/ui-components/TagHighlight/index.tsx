ccaece97a70e32522d394d539a17f730d504e3c7
phelpa <pliniohelpa@gmail.com>
2022-07-20 21:03:44 -0300

    feat: add colors

--- src/pages/ui-components/TagHighlight/index.tsx
+++ src/pages/ui-components/TagHighlight/index.tsx
@@ -1,33 +1,59 @@
-import styled, { css, cx, StyledProp } from '@eduzz/houston-styles';
+import styled, { css, cx, HoustonThemeProps, StyledProp } from '@eduzz/houston-styles';
+
+import warning from '../utils/warning';
 
 export interface TagHighlightProps extends StyledProp, React.HTMLAttributes<HTMLSpanElement> {
   children: string;
-  color: string;
+  color?: string;
   variant?: 'outlined' | 'filled';
 }
 
-const TagHighlight = ({ children, className, color, variant = 'filled', ...rest }: TagHighlightProps) => (
+const TagHighlight = ({ children, className, variant = 'filled', ...rest }: TagHighlightProps) => (
   <span className={cx(className, `--${variant}`)} {...rest}>
     {children}
   </span>
 );
 
+function getColor(theme: HoustonThemeProps, color?: string) {
+  if (!color) return;
+
+  if (color === 'inherit') {
+    return 'inherit';
+  }
+
+  if (color === 'primary') {
+    return theme.brandColor.primary.pure;
+  }
+
+  const [themeColor, level, variable] = color.split('.');
+  const result = theme[themeColor]?.[level]?.[variable];
+
+  if (!result) {
+    warning('Typography', `invalid color ${color}`);
+  }
+
+  return result;
+}
+
 const MAX_HEIGHT_IN_PIX = 22;
 
-export default styled(TagHighlight, { label: 'houston-tag-highlight' })(({ theme }) => {
+export default styled(TagHighlight, { label: 'houston-tag-highlight' })(({ theme, color }) => {
+  console.log(color, 'color');
   return css`
-    background-color: ${theme.neutralColor.high.medium};
+    background-color: ${color ? getColor(theme, color) : theme.neutralColor.high.medium};
     border-radius: ${theme.border.radius.pill};
     padding: ${theme.spacing.stack.quarck} ${theme.spacing.inline.xxxs};
     display: inline-flex;
     align-items: center;
     font-size: ${theme.font.size.xxs};
+    color: ${color === 'brandColor.primary.pure' ? theme.neutralColor.high.pure : theme.neutralColor.low.pure};
     line-height: ${theme.line.height.default};
     max-height: ${theme.pxToRem(MAX_HEIGHT_IN_PIX)}rem;
 
     &.--outlined {
       border: ${theme.border.width.xs} solid;
-      border-color: ${theme.neutralColor.low.pure};
+      border-color: ${getColor(theme, color)};
+      color: ${getColor(theme, color)};
       background-color: ${theme.hexToRgba(theme.neutralColor.low.pure, theme.opacity.level[0])};
     }
   `;
