4cd3d1c439c03081215a71f7d310ecc65a15ca2f
phelpa <pliniohelpa@gmail.com>
2022-07-20 21:39:44 -0300

    feat: add docs

--- src/pages/ui-components/TagHighlight/index.tsx
+++ src/pages/ui-components/TagHighlight/index.tsx
@@ -14,7 +14,7 @@ const TagHighlight = ({ children, className, variant = 'filled', ...rest }: TagH
   </span>
 );
 
-function getColor(theme: HoustonThemeProps, color?: string) {
+function getColor(theme: HoustonThemeProps, color: string) {
   if (!color) return;
 
   if (color === 'inherit') {
@@ -37,24 +37,27 @@ function getColor(theme: HoustonThemeProps, color?: string) {
 
 const MAX_HEIGHT_IN_PIX = 22;
 
-export default styled(TagHighlight, { label: 'houston-tag-highlight' })(({ theme, color }) => {
-  console.log(color, 'color');
-  return css`
-    background-color: ${color ? getColor(theme, color) : theme.neutralColor.high.medium};
-    border-radius: ${theme.border.radius.pill};
-    padding: ${theme.spacing.stack.quarck} ${theme.spacing.inline.xxxs};
-    display: inline-flex;
-    align-items: center;
-    font-size: ${theme.font.size.xxs};
-    color: ${color === 'brandColor.primary.pure' ? theme.neutralColor.high.pure : theme.neutralColor.low.pure};
-    line-height: ${theme.line.height.default};
-    max-height: ${theme.pxToRem(MAX_HEIGHT_IN_PIX)}rem;
+export default styled(TagHighlight, { label: 'houston-tag-highlight' })(
+  ({ theme, color = 'neutralColor.high.medium' }) => {
+    const isDefaultColor = color === 'neutralColor.high.medium';
 
-    &.--outlined {
-      border: ${theme.border.width.xs} solid;
-      border-color: ${getColor(theme, color)};
-      color: ${getColor(theme, color)};
-      background-color: ${theme.hexToRgba(theme.neutralColor.low.pure, theme.opacity.level[0])};
-    }
-  `;
-});
+    return css`
+      background-color: ${getColor(theme, color)};
+      border-radius: ${theme.border.radius.pill};
+      padding: ${theme.spacing.stack.quarck} ${theme.spacing.inline.xxxs};
+      display: inline-flex;
+      align-items: center;
+      font-size: ${theme.font.size.xxs};
+      color: ${color === 'brandColor.primary.pure' ? theme.neutralColor.high.pure : theme.neutralColor.low.pure};
+      line-height: ${theme.line.height.default};
+      max-height: ${theme.pxToRem(MAX_HEIGHT_IN_PIX)}rem;
+
+      &.--outlined {
+        border: ${theme.border.width.xs} solid;
+        border-color: ${isDefaultColor ? theme.neutralColor.low.pure : getColor(theme, color)};
+        color: ${isDefaultColor ? theme.neutralColor.low.pure : getColor(theme, color)};
+        background-color: ${theme.hexToRgba(theme.neutralColor.low.pure, theme.opacity.level[0])};
+      }
+    `;
+  }
+);
