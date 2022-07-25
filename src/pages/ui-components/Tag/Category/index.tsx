da7e6eea7d7cbfb32239c0320111cfd18e42d58d
phelpa <pliniohelpa@gmail.com>
2022-07-25 17:26:02 -0300

    feat: add icon prop, fix toastr docs

--- src/pages/ui-components/Tag/Category/index.tsx
+++ src/pages/ui-components/Tag/Category/index.tsx
@@ -1,12 +1,18 @@
 import styled, { css, StyledProp } from '@eduzz/houston-styles';
 
-export interface CategoryProps extends StyledProp, React.HTMLAttributes<HTMLSpanElement> {
-  children: React.ReactNode;
+export interface TagCategoryProps extends StyledProp, React.HTMLAttributes<HTMLSpanElement> {
+  children: string;
+  icon?: React.ReactNode;
 }
 
-const Category = ({ children, ...rest }: CategoryProps) => <span {...rest}>{children}</span>;
+const TagCategory = ({ children, icon, ...rest }: TagCategoryProps) => (
+  <span {...rest}>
+    {icon}
+    {children}
+  </span>
+);
 
-export default styled(Category, { label: 'houston-tag-category' })(({ theme }) => {
+export default styled(TagCategory, { label: 'houston-tag-category' })(({ theme }) => {
   return css`
     font-size: ${theme.font.size.xxs};
     font-weight: ${theme.font.weight.regular};
