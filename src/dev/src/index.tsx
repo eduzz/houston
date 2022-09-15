0856ab8ff62f3a966965a0edb7eafda6b362b3e3
phelpa <pliniohelpa@gmail.com>
2022-09-14 23:01:21 -0300

    feat: add docs

--- src/dev/src/index.tsx
+++ src/dev/src/index.tsx
@@ -5,4 +5,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+  document.getElementById('root')
+);
