bcf847de0739b39ca25e4e2aaa20385cd1cc94dd
danieloprado <danielprado.ad@gmail.com>
2023-03-07 13:36:37 -0300

    chore: v1.5.8

--- src/pages/forms/Textarea/index.tsx
+++ src/pages/forms/Textarea/index.tsx
@@ -1,33 +1,38 @@
 import * as React from 'react';
 
-import { Input as AntdInput, InputRef } from 'antd';
+import { Input as AntdInput } from 'antd';
 import type { TextAreaProps as AntdTextAreaProps, TextAreaRef } from 'antd/lib/input/TextArea';
 
 import withForm, { WithFormProps } from '../Form/withForm';
 
 export interface TextAreaProps
-  extends Omit<AntdTextAreaProps, 'onChange' | 'onBlur' | 'onPressEnter'>,
-    WithFormProps<InputRef> {
+  extends Omit<AntdTextAreaProps, 'onChange' | 'onBlur'>,
+    WithFormProps<TextAreaRef>,
+    React.RefAttributes<TextAreaRef> {
   onChange?: (value: string | number | null | undefined, event: React.ChangeEvent<HTMLTextAreaElement>) => any;
   onBlur?: (value: string | number | null | undefined, event: React.FocusEvent<HTMLTextAreaElement>) => any;
 }
 
-const Textarea = React.forwardRef<TextAreaRef, TextAreaProps>(({ value, onChange, onBlur, ...props }, ref) => {
-  const handleChange = React.useCallback(
-    (e: React.ChangeEvent<HTMLTextAreaElement>) => {
-      onChange && onChange(e.target.value, e);
-    },
-    [onChange]
-  );
+const Textarea: React.ForwardRefExoticComponent<TextAreaProps> = React.forwardRef<TextAreaRef, TextAreaProps>(
+  ({ value, onChange, onBlur, ...props }, ref) => {
+    const handleChange = React.useCallback(
+      (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+        onChange && onChange(e.target.value, e);
+      },
+      [onChange]
+    );
 
-  const handleBlur = React.useCallback(
-    (e: React.FocusEvent<HTMLTextAreaElement>) => {
-      onBlur && onBlur(e.target.value, e);
-    },
-    [onBlur]
-  );
+    const handleBlur = React.useCallback(
+      (e: React.FocusEvent<HTMLTextAreaElement>) => {
+        onBlur && onBlur(e.target.value, e);
+      },
+      [onBlur]
+    );
 
-  return <AntdInput.TextArea ref={ref} value={value} onChange={handleChange} onBlur={handleBlur} rows={4} {...props} />;
-});
+    return (
+      <AntdInput.TextArea ref={ref} value={value} onChange={handleChange} onBlur={handleBlur} rows={4} {...props} />
+    );
+  }
+);
 
-export default withForm(Textarea as React.ComponentType<TextAreaProps>);
+export default withForm(Textarea);
